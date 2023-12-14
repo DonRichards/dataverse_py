84aea4769874062bbf77bf839ed02cc23b77553f
Don Richards <donrichards@jhu.edu>
2023-12-14 15:16:27 -0500

    Add help and pretty to fits

--- fits_extract.py
+++ fits_extract.py
@@ -1,23 +1,30 @@
 #!/usr/bin/env python3
 # python -m pip install astropy
 
+import argparse
 import astropy.io.fits as fits
+import json
 
 def extract_fits_metadata(file_path):
-    """
-    Extracts all metadata from a FITS file.
-
-    :param file_path: Path to the FITS file.
-    :return: Dictionary containing metadata.
-    """
     with fits.open(file_path) as hdul:
         metadata = {}
         for i, hdu in enumerate(hdul):
             header = hdu.header
-            metadata[f"HDU_{i}"] = {key: header[key] for key in header.keys()}
+            metadata[f"HDU_{i}"] = {key: str(header[key]) for key in header.keys()}
         return metadata
 
-# Example usage
-file_path = 'sample_fits/detectors.LD.Flow.LD.Occupancy.austria.graz.fits'
-metadata = extract_fits_metadata(file_path)
-print(metadata)
+def main():
+    parser = argparse.ArgumentParser(description="Extract metadata from a FITS file.")
+    parser.add_argument("file_path", help="Path to the FITS file.")
+    parser.add_argument("--pretty", action="store_true", help="Pretty-print the JSON output.")
+    args = parser.parse_args()
+
+    metadata = extract_fits_metadata(args.file_path)
+
+    if args.pretty:
+        print(json.dumps(metadata, indent=4))
+    else:
+        print(metadata)
+
+if __name__ == "__main__":
+    main()
